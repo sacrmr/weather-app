83dba8f60208e866e1ae23aefe9b2a726661aa89
sacrmr <106908810+sacrmr@users.noreply.github.com>
2022-07-29 11:38:48 +0200

    Added relevant icon from API

--- src/app.js
+++ src/app.js
@@ -28,14 +28,21 @@ function showTemp(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon-primary");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   conditionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    ` http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "ca5b7f2d8535cadcd8f5264fda720408";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Vienna&units=metric&appid=${apiKey}`;
+let city = "Vienna";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 axios.get(apiUrl).then(showTemp);
